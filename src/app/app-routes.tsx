a85aa5f6d93f8aa2f3f3746c41f9dfaee1c671e0
MATEUSZ MATYSZKOWICZ <mateusz.matyszkowicz@boldare.com>
2021-09-28 20:52:10 +0200

    chore: Setup redux, redux-persist, redux-observable

--- src/app/app-routes.tsx
+++
@@ -1,9 +0,0 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
-export const AppRoutes = () => {
-  return (
-    <Router>
-      <Route path={"/"} exact render={() => <div>Main</div>} />
-    </Router>
-  );
-};
